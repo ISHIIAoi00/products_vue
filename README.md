e262e1d0eb742761862f1abb7bb362bb14dd2223
ISHIIAoi00 <73536128+ISHIIAoi00@users.noreply.github.com>
2021-01-27 18:32:14 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# products_vue
-vue コンポーネントを使用した商品一覧ページ
+# serve
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <product-list v-bind:products="products"></product-list>
+  </div>
+</template>
+
+<script>
+import productList from './components/product-list.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'product-list': productList
+  },
+  data: function() {
+    return {
+      products: [
+        { id: 1, name: 'りんご', price: 1000, image: require('./assets/img/01.png'), delv: 0,isSale: true},
+        { id: 2, name: 'さくらんぼ', price: 1050, image: require('./assets/img/02.png'), delv: 0,isSale: true},
+        { id: 3, name: 'オレンジ', price: 980, image: require('./assets/img/03.png'), delv: 240,isSale: true},
+        { id: 4, name: 'レモン', price: 1200, image: require('./assets/img/04.png'), delv: 0,isSale: true},
+        { id: 5, name: 'バナナ', price: 1030, image: require('./assets/img/05.png'), delv: 0,isSale: false},
+        { id: 6, name: 'メロン', price: 2000, image: require('./assets/img/06.png'), delv: 0,isSale: false},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background: #fff;
+  color: #000;
+}
+</style>
